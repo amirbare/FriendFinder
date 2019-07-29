02e499fad30229acdbca6b1a860229651dfab553
Amir Bar <havainc@gmail.com>
2019-07-28 23:06:53 -0700

    finished routing and html

---
+++ app/data/friends.js
@@ -0,0 +1,71 @@
+var friends = [
+
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            "1",
+            "2",
+            "3",
+            "4",
+            "1",
+            "1",
+            "4",
+            "5",
+            "3",
+            "5"
+          ]
+      },
+      {
+        "name":"Jon",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+          ]
+      },   {
+        "name":"Jen",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            "5",
+            "1",
+            "3",
+            "1",
+            "5",
+            "3",
+            "2",
+            "2",
+            "4",
+            "2"
+          ]
+      },   
+      
+      {
+        "name":"Britney",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            "2",
+            "3",
+            "3",
+            "4",
+            "2",
+            "5",
+            "1",
+            "2",
+            "4",
+            "2"
+          ]
+      }
+
+
+]
+
+module.exports = friends; 
